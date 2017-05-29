ed732fd15a8f11239f2a525cd1afa398c08db60c
Tajbeed-Chowdhury <tajbeedchowdhury@gmail.com>
2017-05-29 13:36:20 +0600

    Website

---
+++ res/x5settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'url' => 'http://tajbeed.github.io',
+	'public_folder' => '',
+	'salt' => '871clik632egs7p8157d735dd78642iv40a94nt4sy06g4e50134s5hy16b',
+	'use_common_email_sender_address' => false,
+	'common_email_sender_addres' => ''
+);
+
+// End of file x5settings.php
